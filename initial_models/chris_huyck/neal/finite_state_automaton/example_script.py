3059c3b2e9e8b7039d62153f648f580d97993611
lungsi <lungsi@users.noreply.github.com>
2019-10-17 09:52:15 +0200

    Update example_script.py

--- initial_models/chris_huyck/neal/finite_state_automaton/example_script.py
+++ initial_models/chris_huyck/neal/finite_state_automaton/example_script.py
@@ -17,8 +17,8 @@ state_cells = runf.create_neurons()
 record = runf.setup_recording(state_cells)
 
 # build FSA and run
-#runf.three_state_fsa(first_spike_gen, second_spike_gen, state_cells)
-#runf.run_fsa(run_duration)
+runf.three_state_fsa(first_spike_gen, second_spike_gen, state_cells)
+runf.run_fsa(run_duration)
 
 # print results
 #runf.print_results(state_cells)
