4ede7b3f0656612da84bf11cb67d7f9887d9a973
lungsi <lungsi@users.noreply.github.com>
2019-10-17 12:40:09 +0200

    Update neal.py

--- initial_models/chris_huyck/neal/finite_state_automaton/neal.py
+++ initial_models/chris_huyck/neal/finite_state_automaton/neal.py
@@ -4,9 +4,10 @@ import pyNN.spiNNaker as spinn
 class NealFunctions(object):
     """Function to isolate differences between different list synapse constructors.
     """
+    def __init__(self, delay):
+        self.delay = delay
     
-    @staticmethod    
-    def nealprojection(pre_neurons, post_neurons, connector_list, inh_exc):
+    def nealprojection(self, pre_neurons, post_neurons, connector_list, inh_exc):
         #projection method for SpiNNaker
         conn_list = spinn.FromListConnector( connector_list )
         spinn.Projection(pre_neurons, post_neurons, conn_list, inh_exc)
