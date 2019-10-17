062b7ffdde3c1b9baa84abad4546d1591754efd2
lungsi <lungsi@users.noreply.github.com>
2019-10-17 12:16:27 +0200

    Update neal.py

--- initial_models/chris_huyck/neal/finite_state_automaton/neal.py
+++ initial_models/chris_huyck/neal/finite_state_automaton/neal.py
@@ -9,4 +9,4 @@ class NealFunctions(object):
     def nealprojection(pre_neurons, post_neurons, connector_list, inh_exc):
         #projection method for SpiNNaker
         conn_list = spinn.FromListConnector( connector_list )
-        spinn.Projection(pre_neurons, post_neurons, conn_list, target= inh_exc)
+        spinn.Projection(pre_neurons, post_neurons, conn_list, inh_exc)
