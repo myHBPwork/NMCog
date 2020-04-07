9e249ecee34cf801dd9e3fc10f30de79dc764caf
lungsi <neuralgraphs@gmail.com>
2020-04-07 18:51:35 +0200

    added v0.2.0 3-Alpha for pip install

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Lungsi'
 author = 'Lungsi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.15 3-Alpha'
+release = '0.2.0 3-Alpha'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
       #"nmcog.spinnaker.cognitiveplanning.nealplanning",
       "nmcog.spinnaker.specialfunction.neal",
       ],
-  version = "0.1.28",
+  version = "0.2.0",
   license="BSD Clause-3",
   description = "Module (Library) for simulation cognitive models in Neuromorphic hardwares.",
   long_description = long_description,
@@ -25,7 +25,7 @@ setup(
   author = "Lungsi Ngwua",
   author_email = "lungsi.ngwua@cnrs.fr",
   url = "https://github.com/myHBPwork/NMCog",
-  download_url = "https://github.com/NMCog/archive/v0.1.28.tar.gz",
+  download_url = "https://github.com/NMCog/archive/v0.2.0.tar.gz",
   keywords = ["NEUROMORPHIC", "COGNITIVE", "SPIKING", "NEURAL NETWORK", "SPINNAKER", "BRAINSCALES"],
   #install_requires=[
   #        "numpy",
